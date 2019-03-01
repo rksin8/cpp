c46629f6d7e33f95806b02bfdcb3e5af126e3442
rksin8 <ranjeetbhu88@gmail.com>
2019-03-02 02:33:39 +1100

    matrix rotation by -90 degree

--- matrix/rotate_by_90.cpp
+++ matrix/rotate_by_90.cpp
@@ -3,14 +3,20 @@
 
 #include "print.h"
 
+
+
+
+
 int main(int argc, char *argv[])
 {
 
     using Matrix = std::vector<std::vector<double>>;
     Matrix v{ {1,5,7},{8,9,4},{2,3,6}};
+    Matrix w  = v;
 
     print(v);
     Matrix result{{0,0,0},{0,0,0},{0,0,0}};
+    Matrix result2{{0,0,0},{0,0,0},{0,0,0}};
 
 
     for (int i = 0; i < v.size(); ++i) {
@@ -21,6 +27,15 @@ int main(int argc, char *argv[])
 
     std::cout << "Matrix after rotation:" << std::endl;
     print(result);
+
+    for (int i = 0; i < w.size(); ++i) {
+        for (int j = 0; j < w[0].size(); ++j) {
+            result2[i][j] = w[j][w[0].size()-i-1];
+        }
+    }
+    std::cout << "Matrix after rotation by -90:" << std::endl;
+    print(result2);
+
     
     return 0;
 }
